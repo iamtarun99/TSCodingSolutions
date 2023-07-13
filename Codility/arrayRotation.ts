3aaf163af9c4ef0ebd697f0ed5153e8351d5b2a7
Tarun Gupta <tarun.gupta093@gmail.com>
2023-07-13 16:18:58 +0530

    arrayRotation

---
+++ Codility/arrayRotation.ts
@@ -0,0 +1,27 @@
+
+/*
+IP: A = [3, 8, 9, 7, 6]
+    K = 3
+OP: [9, 7, 6, 3, 8]
+*/
+
+function solution(A: number[], K: number): number[] {
+    //Solution-1
+    // while(K>0){
+    //     A.unshift(A.pop());
+    //     K--;
+    // }
+    // return A;
+
+    //Solution-2
+    const B: number[] = [];
+    for(let i=0; i<A.length; i++){
+        B[getIndex(i,K, A.length)] = A[i];
+    }
+    return B;
+}
+
+function getIndex(index: number, num: number, maxLen: number):number{
+    let targetIndex = index+num;
+    return targetIndex>=maxLen?targetIndex-maxLen:targetIndex
+}
